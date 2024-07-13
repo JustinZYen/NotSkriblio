482fd86534d991c193ad6abe099395d53325c2f0
Aaron Pang <aaroncwpang@gmail.com>
2024-07-12 21:19:27 -0700

    s

--- static/script.js
+++ static/script.js
@@ -74,3 +74,19 @@ $("#chat-input").on("keypress",(event)=>{
         $("#chat-input").val("");
     }
 })
+
+const socket = io();
+
+document.addEventListener("click",(e)=> {
+    socket.emit('chat message', e.clientX);
+});
+
+socket.on('chat message', (msg) => {
+    /*
+    const item = document.createElement('li');
+    item.textContent = msg;
+    messages.appendChild(item);
+    window.scrollTo(0, document.body.scrollHeight);
+    */
+   console.log("message received");
+});
