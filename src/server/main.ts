209e527d9104cf2d5f23c0c94966d5db952578e8
justzyen <justzyen@gmail.com>
2025-03-28 16:12:41 -0700

    Take out unneeded site components

--- src/server/main.ts
+++ src/server/main.ts
@@ -8,10 +8,6 @@ import { ClientToServerEvents, InterServerEvents, RoomJoinResult, ServerToClient
 
 const app = express();
 
-app.get("/hello", (_, res) => {
-  res.send("Hello Vite + React + TypeScript!");
-});
-
 const server = ViteExpress.listen(app, 3000, () =>
   console.log("Server is listening on port 3000..."),
 );
